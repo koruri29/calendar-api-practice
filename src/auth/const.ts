44e817773b820b047cd9ea91c363764b1320cf98
JUNK329\kmaki <koruri821@gmail.com>
2025-08-17 15:00:04 +0900

    update: (auth)スコープをフルアクセスに修正(CRUD機能追加予定のため)

--- src/auth/const.ts
+++ src/auth/const.ts
@@ -5,5 +5,5 @@ import * as path from "path";
 export const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
-export const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+export const SCOPES = ["https://www.googleapis.com/auth/calendar"]
 export const TOKEN_PATH = path.join(__dirname, "../../token.json")
