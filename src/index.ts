d4a8d4dcc7330be63bb6eab3998ab56b15c5182e
JUNK329\kmaki <koruri821@gmail.com>
2025-08-17 16:19:55 +0900

    feat: 予定の登録機能 追加
    fix: (登録)日時で登録の際の入力時を間違えていた
    
    fix: (auth)token.jsonの保存場所を修正

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as dotenv from "dotenv";
 import { authorize } from "./auth/authorize.ts";
-import { listUpcomingEvents, type ListOptions } from "./get/listUpcomingEvents.ts";
 import readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
+import { createEvent } from "./create/createEvent.ts";
+import type { RawCreateValues } from "./types/create.ts";
+import { validateCreateInputs } from "./create/validateCreateInputs.ts";
+import { formatCreateEvent } from "./create/formatCreateEvent.ts";
 
 
 // .env呼び出し
@@ -16,57 +19,77 @@ async function main() {
     const authClient = await authorize()
 
 
-    const options: ListOptions = {}
+    // 参照
+    // const options: ListOptions = {}
 
-    const rl = readline.createInterface({ input, output })
-    const advanced = await rl.question('詳細検索を行いますか？(no): ')
+    // const rl = readline.createInterface({ input, output })
+    // const advanced = await rl.question('詳細検索を行いますか？[no]: ')
 
-    if (
-      advanced === 'y' ||
-      advanced === 'Y' ||
-      advanced === 'yes' ||
-      advanced === 'YES'
-    ) {
-      const today = getTodayYYYYMMDD()
-      const startStr = await rl.question(`取得開始日(${today}): `)
-      const endStr = await rl.question('取得終了日(none): ')
-      const maxStr = await rl.question('最大件数(10): ')
-      const keyword = await rl.question('キーワード(none): ')
+    // if (
+    //   advanced === 'y' ||
+    //   advanced === 'Y' ||
+    //   advanced === 'yes' ||
+    //   advanced === 'YES'
+    // ) {
+    //   const today = getTodayYYYYMMDD()
+    //   const startStr = await rl.question(`取得開始日[${today}]: `)
+    //   const endStr = await rl.question('取得終了日[none]: ')
+    //   const maxStr = await rl.question('最大件数[10]: ')
+    //   const keyword = await rl.question('キーワード[none]: ')
 
-      if (startStr) {
-        const inputDate = new Date(startStr)
-        if (!isNaN(inputDate.getTime())) {
-          options.timeMin = inputDate
-        }
-      }
-      if (endStr) {
-        const inputDate = new Date(endStr)
-        if (!isNaN(inputDate.getTime())) {
-          options.timeMax = inputDate
-        }
-      }
-      if (maxStr) {
-        const max = options.maxResults = parseInt(maxStr, 10)
-        options.maxResults = isNaN(max) ? DEFAULT_MAX_RESULTS : max
-      }
-      if (keyword) options.q = keyword
+    //   if (startStr) {
+    //     const inputDate = new Date(startStr)
+    //     if (!isNaN(inputDate.getTime())) {
+    //       options.timeMin = inputDate
+    //     }
+    //   }
+    //   if (endStr) {
+    //     const inputDate = new Date(endStr)
+    //     if (!isNaN(inputDate.getTime())) {
+    //       options.timeMax = inputDate
+    //     }
+    //   }
+    //   if (maxStr) {
+    //     const max = options.maxResults = parseInt(maxStr, 10)
+    //     options.maxResults = isNaN(max) ? DEFAULT_MAX_RESULTS : max
+    //   }
+    //   if (keyword) options.q = keyword
+    // }
+
+    // rl.close()
+
+    // await listUpcomingEvents(authClient, options)
+
+
+    // 登録
+    const rl2 = readline.createInterface({ input, output })
+
+    const inputs: RawCreateValues = {}
+
+    console.log('予定の登録を行います')
+    inputs.summary = await rl2.question('タイトル(必須): ')
+    inputs.description = await rl2.question('内容: ')
+    inputs.location = await rl2.question('場所: ')
+    inputs.startDate = await rl2.question(`開始日(必須、yyyy-mm-dd): `)
+    inputs.startTime = await rl2.question('開始時刻(HH:mm): ')
+    inputs.endDate = await rl2.question('終了日(yyyy-mm-dd): ')
+    inputs.endTime = await rl2.question('終了時刻(HH:mm): ')
+
+    rl2.close()
+
+
+    const validated = validateCreateInputs(inputs)
+    if (!validated.ok) {
+      console.log('入力値が不正です。', validated.message || '')
+      return
     }
 
-    rl.close()
-
-    await listUpcomingEvents(authClient, options)
+    const formattedEvent = formatCreateEvent(inputs)
+    await createEvent(authClient, formattedEvent)
 
   } catch (error) {
     console.error(error)
   }
-}
-
-function getTodayYYYYMMDD(): string {
-  const today = new Date();
-  const yyyy = today.getFullYear();
-  const mm = String(today.getMonth() + 1).padStart(2, "0"); // 月は0始まりなので+1
-  const dd = String(today.getDate()).padStart(2, "0");
-  return `${yyyy}-${mm}-${dd}`;
 }
 
 main()
